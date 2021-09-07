881a251bbbc3c8ee8616d58cf723b56412bcebf9
KJ <abc8339265@gmail.com>
2021-09-07 16:37:43 +0900

    spring project initialization

---
+++ src/main/java/com/kj/WhatShouldIEatToday/WhatShouldIEatTodayApplication.java
@@ -0,0 +1,13 @@
+package com.kj.WhatShouldIEatToday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatShouldIEatTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatShouldIEatTodayApplication.class, args);
+	}
+
+}
